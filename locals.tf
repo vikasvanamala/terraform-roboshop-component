2bc0a715aff1848218932096b8e09019cd4e7539
vikas-vanamala <vikasvanamala008@gmail.com>
2026-04-28 00:21:15 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -10,10 +10,10 @@ locals {
   sg_id = data.aws_ssm_parameter.sg_id
   private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
   private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
-  tg_port = "${var.component}" == frontend ? 80 : 8080
-  health_check_path = "${var.component}" == frontend ? "/" : "/health"
+  tg_port = "${var.component}" == "frontend" ? 80 : 8080
+  health_check_path = "${var.component}" == "frontend" ? "/" : "/health"
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
-  alb_listener_arn = "${var.component}" == frontend ? "local.frontend_alb_listener_arn" : "local.backend_alb_listener_arn"
-  host_context = "${var.component}" == frontend ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
+  alb_listener_arn = "${var.component}" == "frontend" ? "local.frontend_alb_listener_arn" : "local.backend_alb_listener_arn"
+  host_context = "${var.component}" == "frontend" ? "${var.project_name}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
 } 
