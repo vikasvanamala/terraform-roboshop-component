c5f67ba3069ff040c512d8243565e75a94887313
vikas-vanamala <vikasvanamala008@gmail.com>
2026-04-28 12:25:37 +0530

    terraform

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_launch_template" "main" {
-  name   = "${local.common_name_suffix}-main"
+  name   = "${local.common_name_suffix}-${var.component}"
   image_id      = aws_ami_from_instance.main.id  # Replace with valid AMI ID
 
   instance_initiated_shutdown_behavior = "terminate"
